bfe0028d593709896fbd30da83829540dd6e64ab
maryam.yazdanirad <maryam.yazdanirad@snapp.cab>
2023-08-06 13:36:48 +0330

    fix(page): initial values for cards

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,69 @@ import CardList from './components/card-list/CardList';
 
 export default function Home() {
 	const monsters = [
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+		{
+			id: 1,
+			url: 1,
+			available: true,
+		},
+		{
+			id: 2,
+			url: 2,
+			available: true,
+		},
+		{
+			id: 3,
+			url: 5,
+			available: true,
+		},
+		{
+			id: 4,
+			url: 4,
+			available: true,
+		},
+		{
+			id: 5,
+			url: 8,
+			available: true,
+		},
+		{
+			id: 6,
+			url: 9,
+			available: true,
+		},
+		{
+			id: 7,
+			url: 1,
+			available: true,
+		},
+		{
+			id: 8,
+			url: 2,
+			available: true,
+		},
+		{
+			id: 9,
+			url: 5,
+			available: true,
+		},
+		{
+			id: 10,
+			url: 4,
+			available: true,
+		},
+		{
+			id: 11,
+			url: 8,
+			available: true,
+		},
+		{
+			id: 12,
+			url: 9,
+			available: true,
+		},
 	];
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+		<main className='flex min-h-screen flex-col items-center justify-between p-10'>
 			{monsters && <CardList monsters={monsters} />}
 		</main>
 	);
