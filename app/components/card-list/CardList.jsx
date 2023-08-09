6c9cab449e796fabc874069a7257a54febf2186c
maryam.yazdanirad <maryam.yazdanirad@snapp.cab>
2023-08-09 14:33:32 +0330

    feat(functionality): the game is working

--- app/components/card-list/CardList.jsx
+++ app/components/card-list/CardList.jsx
@@ -1,12 +1,91 @@
+'use client';
+import { useEffect, useRef, useState } from 'react';
 import Card from '../card/Card';
 import style from './CardList.module.css';
 
 export default function CardList({ monsters }) {
+	const [fliped, setFliped] = useState([]);
+	const [score, setScore] = useState(0);
+	const [matchedItems, setMatchedItems] = useState([]);
+	const [isFirstTime, setIsFirstTime] = useState(true);
+	const [modifiedMonsters, setModifiedMonsters] = useState([]);
+	const [status, setStatus] = useState('show');
+
+	const timeout = useRef(null);
+
+	const evaluate = (monster) => {
+		setFliped([...fliped, monster]);
+		if (fliped[0].url === monster.url) {
+			setMatchedItems([...matchedItems, monster]);
+			setScore(score + 1);
+			setModifiedMonsters(
+				modifiedMonsters.map((monster) => {
+					if (monster.url === fliped[0].url) {
+						return {
+							...monster,
+							available: false,
+						};
+					} else {
+						return monster;
+					}
+				})
+			);
+			return;
+		}
+		// Flip cards after a 500ms duration
+		timeout.current = setTimeout(() => {
+			setFliped([]);
+		}, 500);
+	};
+
+	const checkIsFlipped = (id) => {
+		const flipedIds = Array.from(fliped, (f) => f.id);
+		return flipedIds.includes(id);
+	};
+
+	function selectCard(monster) {
+		if (isFirstTime) {
+			setFliped([monster]);
+			setIsFirstTime(false);
+		} else {
+			setTimeout(evaluate(monster), 500);
+			// clearTimeout(timeout.current);
+			setIsFirstTime(true);
+		}
+	}
+
+	let cardsTimeout;
+
+	function tryAgain() {
+		setModifiedMonsters(monsters.toSorted(() => 0.5 - Math.random()));
+		setMatchedItems([]);
+		setStatus('show');
+		clearTimeout(cardsTimeout);
+		cardsTimeout = setTimeout(() => {
+			setStatus('hide');
+		}, 3000);
+		setScore(0);
+		setFliped([]);
+	}
+
 	return (
-		<div className={style.cardList}>
-			{monsters.map((id) => (
-				<Card key={id} id={id} />
-			))}
-		</div>
+		<>
+			<button onClick={tryAgain} className={style.button}>
+				{modifiedMonsters.length ? 'Shuffle' : 'Start'}
+			</button>
+			<h2 className='flex-auto'>Score: {score}</h2>
+			<br />
+			<div className={style.cardList}>
+				{modifiedMonsters.map((m, index) => (
+					<Card
+						key={index}
+						monster={m}
+						onClick={() => selectCard(m)}
+						isFlipped={checkIsFlipped(m.id)}
+						status={status}
+					/>
+				))}
+			</div>
+		</>
 	);
 }
--- app/components/card/Card.jsx
+++ app/components/card/Card.jsx
@@ -1,16 +1,25 @@
 import Image from '@/node_modules/next/image';
+import { useEffect, useState } from 'react';
 import style from './Card.module.css';
 
-const Card = ({ id }) => (
-	<div className={style.cardContainer}>
-		<Image
-			alt='monster'
-			className={style.cardImg}
-			width='180'
-			height='180'
-			src={`https://robohash.org/${id}?set=set2size=180x180`}
-		/>
-	</div>
-);
-
-export default Card;
+export default function Card({ monster, onClick, isFlipped, status }) {
+	return (
+		<div
+			className={`${style.cardContainer} ${
+				!monster.available && style.matched
+			} ${style.status}`}>
+			<Image
+				onClick={() => {
+					onClick();
+				}}
+				alt='monster'
+				className={`${style.cardImg} ${
+					(isFlipped || status === 'show') && style.show
+				}`}
+				width='180'
+				height='180'
+				src={`https://robohash.org/${monster.url}?set=set2size=180x180`}
+			/>
+		</div>
+	);
+}
